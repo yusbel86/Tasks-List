0217a74def8a464626584ca3ded3c5db47fff9e9
yusbel86 <yusbel1086@gmail.com>
2020-02-11 00:14:17 +0000

    Add event

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [contenido, setContenido] = useState();
+	const [display, setDisplay] = useState();
+
+	function click(e) {
+		setContenido(e.target.value);
+	}
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<input onChange={e => click(e)} />
+			<br />
+			<button
+				type="button"
+				className="btn btn-primary"
+				onClick={() => setDisplay(contenido)}>
+				Primary
+			</button>
+
+			<h1>{contenido}</h1>
+			<h1>{display}</h1>
 		</div>
 	);
 }
